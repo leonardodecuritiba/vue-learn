67dfbbf3ce75ad7dafe9e865bac5a0270943b278
silva.zanin@gmail.com <silva.zanin@gmail.com>
2024-05-21 23:18:30 -0300

    Implementing vue-route

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Footer</p>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <router-link>
+      <img src="" id="logo" />
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
